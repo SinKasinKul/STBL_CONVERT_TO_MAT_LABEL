a81f35ed2933840ad9cbab6ba01e989c5d11c3cd
Kasin Kuljiang <77579313+SinKasinKul@users.noreply.github.com>
2022-12-07 16:02:20 +0700

    Add project files.

---
+++ STBL_CONVERT_TO_MAT_LABEL/MainFrom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STBL_CONVERT_TO_MAT_LABEL
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+        private void MainFrom_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+                pictureBox1.Image = qrcode.Draw(textBox1.Text, 70);
+            }
+        }
+    }
+}
